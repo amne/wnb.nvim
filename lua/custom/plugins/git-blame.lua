f6d6181d409f3e8f8d3e31e7a8feef76d38905f5
Cornel Cruceru <amne@amne.me>
2025-02-27 09:58:20 +0200

    put git blame in tabline

--- lua/custom/plugins/git-blame.lua
+++ lua/custom/plugins/git-blame.lua
@@ -1,3 +1,20 @@
 return {
   'f-person/git-blame.nvim',
+  event = "VeryLazy",
+  opts = {
+    message_template = "<<sha>> • <date> • <author> • • <summary>",
+    date_format = "%Y-%m-%d %H:%M:%S",
+  },
+  config = function ()
+    vim.g.gitblame_display_virtual_text = 0 -- Disable virtual text
+    vim.g.gitblame_message_template = "<<sha>> • <date> • <author> • • <summary>"
+    local git_blame = require('gitblame')
+    require('lualine').setup({
+      tabline = {
+        lualine_b = {
+          { git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available }
+        }
+      }
+    })
+  end
 }
